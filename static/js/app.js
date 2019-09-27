87e82cd1ce064d41567ddc5be49bbe677b1606ba
junwoo77lee <47956198+junwoo77lee@users.noreply.github.com>
2019-09-26 23:09:08 -0600

    add functions which draw a map and arcs between airports

--- static/js/app.js
+++ static/js/app.js
@@ -1,21 +1,35 @@
-function timeConverter(UNIX_timestamp){
+function timeConverter(UNIX_timestamp) {
     var a = new Date(UNIX_timestamp * 1000);
-    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
     var year = a.getFullYear();
     var month = months[a.getMonth()];
     var date = a.getDate();
     var hour = a.getHours();
     var min = a.getMinutes();
     var sec = a.getSeconds();
-    var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+    var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec;
     return time;
-  }
+}
 
+
+const svgWidth = 960;
+const svgHeight = 800;
+const rotated = 100;
+
+
+// Geo Projection
+const projection = d3.geoMercator() //.geoAlbersUsa()
+    .scale(1000)
+    .translate([svgWidth / 2, svgHeight / 2])
+    .rotate([rotated, -35, 0]);
+
+const path = d3.geoPath(projection);
 
 
 urlTimeTable = '/get-timetable';  // departure, arrival airports, flight number, status
 urlTracker = '/get-flighttracker'; // flight coordinates, status
 urlAirport = '/get-airportinfo'; // airport coordinates
+urlStateJson = '/get-statejson'; // GeoJSon for US states
 
 
 // TIME TABLE
@@ -79,13 +93,24 @@ urlAirport = '/get-airportinfo'; // airport coordinates
 //  'codeIso2Country': 'US',
 //  'codeIataCity': 'AAF'}
 
+const svg = d3.select('#map').append('svg')
+    .attr('viewBox', `0 0 ${svgWidth} ${svgHeight}`)
+    .attr('preserveAspectRatio', 'xMinYMin');
+
+const map = svg.append('g')
+// .attr('transform', `translate(${svgWidth / 2}, ${svgHeight / 2})`);
+
+
+
 Promise.all([d3.json(urlTimeTable),
 d3.json(urlTracker),
-d3.json(urlAirport)])
-    .then(([timeTable, actualFlightData, airportInfo]) => {
+d3.json(urlAirport),
+d3.json(urlStateJson)])
+    .then(([timeTable, actualFlightData, airportInfo, usState]) => {
         // console.log(timeTable);
         // console.log(actualFlightData);
         // console.log(airportInfo);
+        // console.log(usState);
 
         // Reorganize airport timetable data into an object
         let masterObj = [];
@@ -114,30 +139,137 @@ d3.json(urlAirport)])
                     item['arrivalLat'] = Number(airport.latitudeAirport);
                     item['arrivalLon'] = Number(airport.longitudeAirport);
                 } else {
-                    
+
                 }
-            }); 
+            });
         });
 
         // Merge with Flight tracker dataset to extract currenct positon, speed, and direction
         actualFlightData.forEach(flight => {
             masterObj.forEach(item => {
                 if ((item.departIata === flight.departure.iataCode) &&
-                   (item.arrivalIata === flight.arrival.iataCode) &&
-                   (item.flightIcao === flight.flight.icaoNumber)) {
-                       item['currentLat'] = flight.geography.latitude;
-                       item['currentLon'] = flight.geography.longitude;
-                       item['currentAlt'] = flight.geography.altitude;
-                       item['currentSpeedH'] = flight.speed.horizontal;
-                       item['currentSpeedV'] = flight.speed.vertical;
-                       item['currentStatus'] = flight.status;
-                       item['currentUpdateAt'] = timeConverter(Number(flight.system.updated));
-                   }
+                    (item.arrivalIata === flight.arrival.iataCode) &&
+                    (item.flightIcao === flight.flight.icaoNumber)) {
+                    item['currentLat'] = flight.geography.latitude;
+                    item['currentLon'] = flight.geography.longitude;
+                    item['currentAlt'] = flight.geography.altitude;
+                    item['currentSpeedH'] = flight.speed.horizontal;
+                    item['currentSpeedV'] = flight.speed.vertical;
+                    item['currentStatus'] = flight.status;
+                    item['currentUpdateAt'] = timeConverter(Number(flight.system.updated));
+                }
             });
         });
 
         console.log(masterObj);
-        
+
+        let currentObj = [];
+        masterObj.forEach(item => {
+            if (item.currentLat) {
+                currentObj.push(item);
+            }
+        });
+
+        // console.log(currentObj);
+
+        drawMap(path, usState.features, masterObj);
+        drawArc(path, masterObj);
 
     });
 
+
+
+// Draw a base map by states
+function drawMap(geoPath, stateFeature, masterData) {
+
+    map.selectAll('path .state')
+        .data(stateFeature)
+        .enter()
+        .append('path')
+        .classed('state', true)
+        .attr("name", function (d) {
+            return d.properties.NAME;
+        })
+        .attr("id", function (d) {  // contry code ex. 'AFG'
+            return d.properties.STATE;
+        })
+        .attr("d", geoPath)
+        .attr('fill', 'none')
+        .attr('stroke', 'black')
+        .attr('stroke-width', '2');
+
+}
+
+// Draw arcs between airports
+function drawArc(geoPath, coordinatesObj) {
+
+    const arcs = svg.append('g')
+
+    var lineStrings = [];
+    coordinatesObj.forEach(function (obj) {
+        lineStrings.push({
+            type: "LineString",
+            coordinates: [[obj.departLon, obj.departLat],
+            [obj.arrivalLon, obj.arrivalLat]],
+            routes: [obj.departIata, obj.arrivalIata],
+            flight: {
+                icaoNumber: obj.flightIcao,
+                departScheduledTime: obj.departSchTime,
+                arrivalScheduledTime: obj.arrivalSchTime,
+                status: obj.status,
+                currentStatus: obj.currentStatus
+            }
+        });
+    });
+
+    // console.log(lineStrings);
+
+    // lineStrings.forEach(d => {
+    //     d.coordinates.forEach(c => {
+    //         if (!c[0] || !c[1]) {
+    //             console.log(d);
+    //         }
+    //     });
+    // });
+
+
+
+    // Add the path
+    arcs.selectAll('path .arcs')
+        .data(lineStrings)
+        .enter()
+        .append('path')
+        .attr('class', 'arcs')
+        .attr('id', d => {
+            return `From: ${d.routes[0]} To: ${d.routes[1]}`
+        })
+        .attr("d", geoPath)
+        .style("fill", "none")
+        .style("stroke", "darkred")
+        .style("stroke-width", 1)
+    
+    updateTooptips(arcs);
+
+}
+
+function updateTooptips(arcGroup) {
+  
+    const toolTip = d3.tip()
+        // .attr('class', 'd3-tip')
+        .offset([180, -100])
+        .html(function(d) {
+            return (`<h5>From: ${d.routes[0]}</h5><br>
+            <h5>To: ${d.routes[1]}</h5>`);
+        });
+
+    arcGroup.call(toolTip);
+
+    arcGroup.on('mouseover', function (data) {
+        toolTip.show(data, this);
+    })
+        .on('mouseout', function (data) {
+            toolTip.hide(data, this);
+        });
+
+    return arcGroup;
+}
